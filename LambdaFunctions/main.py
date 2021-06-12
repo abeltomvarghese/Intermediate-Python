0d648d3d7eb9642a123bd0cae350170c7c2b972e
Abel Tom Varghese <abeltomvarghese1998@gmail.com>
2021-06-12 22:22:49 +0100

    reviewed map, filter & reduce

--- LambdaFunctions/main.py
+++ LambdaFunctions/main.py
@@ -1,3 +1,5 @@
+def squareMe(x):
+	return x**2
 if __name__ == '__main__':
 
 	add10 = lambda x: x + 10
@@ -17,9 +19,17 @@ if __name__ == '__main__':
 	print(sortedBySum)
 
 	a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-	b = map(lambda x: x**2 ,a)
+	b = map(lambda x: x**2 ,a) #ALTERNATIVE: b = map(squareMe, a)
 	print(list(b))
 
 	c = [x**2 for x in range(11)] #ALTERNATIVE: [x**2 for x in a]
 	print(c)
 
+	print("USING FILTER")
+	d = filter(lambda x: x%2==0, a) #ALTERNATIVE: [x for x in a if x%2==0]
+	print(list(d))
+
+	from functools import reduce
+	product_a = reduce(lambda x,y: x*y, a)
+	print("USING REDUCE")
+	print(product_a)
